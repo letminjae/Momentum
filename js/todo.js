e5b66e44ffe7a4d1006c9ef0c66e522902a4c5cc
letminjae <cmjj0824@naver.com>
2022-06-20 20:51:36 +0900

    ADD : Loading todos & Saving toDos in Localstorage

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,27 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos= [];
+
+const TODOS_KEY = "todos"
+
+function saveTodos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
   li.appendChild(span);
+  li.appendChild(button);
   span.innerText = newTodo;
   toDoList.appendChild(li);
 }
@@ -14,7 +31,16 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveTodos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if(savedTodos !== null){
+    const parsedToDos = JSON.parse(savedTodos); // Array
+    parsedToDos.forEach(paintToDo);
+}
