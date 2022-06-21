f8d597292159a7a475f32c3488e2b3a0b32333c0
letminjae <cmjj0824@naver.com>
2022-06-21 21:13:11 +0900

    FEAT : Deleting toDos in todo.js

--- js/todo.js
+++ js/todo.js
@@ -2,7 +2,7 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-const toDos= [];
+let toDos= [];
 
 const TODOS_KEY = "todos"
 
@@ -13,17 +13,20 @@ function saveTodos(){
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveTodos();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteTodo);
   li.appendChild(span);
   li.appendChild(button);
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   toDoList.appendChild(li);
 }
 
@@ -31,8 +34,12 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now()
+  }
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveTodos();
 }
 
@@ -42,5 +49,6 @@ const savedTodos = localStorage.getItem(TODOS_KEY);
 
 if(savedTodos !== null){
     const parsedToDos = JSON.parse(savedTodos); // Array
+    toDos = parsedToDos;
     parsedToDos.forEach(paintToDo);
 }
